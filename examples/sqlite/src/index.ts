d9d75fbd0d38e321649fdf2ea25ad005e424f5dc
Eddie <thfai2000@gmail.com>
2021-05-28 14:23:46 +0800

    relation allow custom filters

--- examples/sqlite/src/index.ts
+++ examples/sqlite/src/index.ts
@@ -1,28 +1,91 @@
-import {configure, Entity, getKnexInstance, Schema, Types} from '../../../dist/'
+import {configure, Entity, Relations, Schema, Types, models, select, raw} from '../../../dist/'
+import {snakeCase} from 'lodash'
 
-class Shop extends Entity{
-    static register(schema: Schema){
-        schema.prop('name', Types.String(255))
+let shopData = [
+  { id: 1, name: 'Shop 1', location: 'Shatin'},
+  { id: 2, name: 'Shop 2', location: 'Yuen Long'},
+  { id: 3, name: 'Shop 3', location: 'Tsuen Wan'},
+  { id: 4, name: 'Shop 4', location: 'Tsuen Wan'},
+  { id: 5, name: 'Shop 5', location: 'Tsuen Wan'}
+]
+
+let productData = [
+  { name: 'Product 1a', shopId: 1},
+  { name: 'Product 1b', shopId: 1},
+  { name: 'Product 2a', shopId: 2},
+  { name: 'Product 2b', shopId: 2}
+]
+
+
+// let colorData = [
+//   {code: 'red'},
+//   {code: 'orange'},
+//   {code: 'yellow'},
+//   {code: 'green'},
+//   {code: 'blue'},
+//   {code: 'black'},
+//   {code: 'white'}
+// ]
+
+
+
+;(async() =>{
+
+    class Shop extends Entity{
+
+      static register(schema: Schema){
+        schema.prop('name', Types.String(100))
         schema.prop('location', Types.String(255))
-        schema.computedProp('products', Types.Array(Product), (shop, injectFunc) => shop.hasMany(Product, 'shopId', injectFunc) )
+        schema.computedProp('products', Types.Array(Product), Relations.has(Product, 'shopId') )
+        schema.computedProp('productCount', Types.Number(), async (shop, applyFilters) => {
+            let p = Product.selector()
+            return applyFilters( select(raw('COUNT(*)') ).from(p.source).where( raw('?? = ??', [shop.id, p._.shopId])), p) 
+        })
+      }
     }
-}
-
-class Product extends Entity{
-    static register(schema: Schema){
-		schema.prop('name', Types.String(255, true))
-		schema.prop('createdAt', Types.Date())
+    
+    class Product extends Entity{
+    
+      static register(schema: Schema){
+        schema.prop('name', Types.String(255, true))
+        schema.prop('createdAt', Types.Date())
         schema.prop('shopId', Types.Number() )
-		schema.computedProp('shop', Types.Object(Shop), (product, applyFilters) => product.belongsTo(Shop, 'shopId', applyFilters) )
-	}
-}
+        // computeProp - not a actual field. it can be relations' data or formatted value of another field. It even can accept arguments...
+        schema.computedProp('shop', Types.Object(Shop), Relations.belongsTo(Shop, 'shopId') )
 
-(async() =>{
+        schema.computedProp('colors', 
+          Types.Array(Color), 
+          Relations.relateThrough(Color, ProductColor, 'productId', 'colorId') 
+        )
+        
+        schema.computedProp('mainColor', 
+          Types.Object(Color), 
+          Relations.relateThrough(Color, ProductColor, 'productId', 'colorId', (stmt, relatedSelector, throughSelector) => {
+            return stmt.andWhereRaw('?? = ?', [throughSelector._.type, 'main'])
+          })
+        )
+      }
+    }
+    
+    class Color extends Entity{
+      static register(schema: Schema){
+        schema.prop('code', Types.String(50))
+      }
+    }
 
-    // configure the orm
+    class ProductColor extends Entity{
+      static register(schema: Schema){
+        schema.prop('productId', Types.Number(false))
+        schema.prop('colorId', Types.Number(false))
+        schema.prop('type', Types.String(50, false))
+      }
+    }
+
     await configure({
-        models: { Shop, Product },
+        models: {Shop, Product, Color},
         createModels: true,
+        entityNameToTableName: (className: string) => snakeCase(className),
+        propNameTofieldName: (propName: string) => snakeCase(propName),
         knexConfig: {
             client: 'sqlite',
             connection: {
@@ -30,20 +93,22 @@ class Product extends Entity{
             }
         }
     })
-    /**
-     * Basic
-     */
-    let myShops = await Shop.find()
-    console.log('HERE:', myShops)
 
-    let shopData = [
-      { name: 'Shop 1', location: 'Shatin'},
-      { name: 'Shop 2', location: 'Yuen Long'},
-      // { id: 3, name: 'Shop 3', location: 'Tsuen Wan'},
-    ]
+
 
     await Promise.all(shopData.map( async(d) => {
-      return await Shop.createOne(d)
+      return await models.Shop.createOne(d)
     }))
+    
+
+    await Promise.all(productData.map( async(d) => {
+      return await models.Product.createOne(d)
+    }))
+
+    let records = await models.Shop.find( async (stmt, root) => {
+        return stmt.select('*', await root.$.productCount())
+    })
+
+    console.log('results', records)
 
 })()
