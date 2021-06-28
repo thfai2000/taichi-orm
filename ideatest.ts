a8f28d99e2046c9fcc4007adc1fe2488dcd9f55a
Eddie Tang <thfai2000@gmail.com>
2021-06-29 02:17:05 +0800

    test

--- ideatest.ts
+++ ideatest.ts
@@ -4,19 +4,25 @@ class Scalar<ResultObject> {
     constructor(private var1: any ) {}
 }
 
-type OmitMethod<T> =  Omit<T, 'find' | 'execute' | 'dataset'>
+type OmitMethod<T> =  Omit<T, keyof Schema>
+
+// type ReplaceCompute<T> = {
+//     [key in keyof T ]: 
+//         T[key] extends ComputeFunction?  ReturnType<T[key]> :
+//         T[key] extends PropertyType<infer R> ? R:
+//         T[key];
+// }
+
+//type Comp = <S, Z, R = Z extends infer C? Pick<Z & S, AllowedNames<C, string | number>> : never>(arg: Z, schema: S) => R;
 
 
 
-type PropsFromQuery<S, T extends QueryOption<S>> = Pick<S, keyof T['props']>
+// type PropsFromQuery<S, T extends QueryOption<S>> = Pick<S, keyof T['props']>
 
 
 // type A = Extract<{hello: number, x: number} | {test: number}, {hello: number, c: number}>
 
 type Base = {hello: boolean}
-
-
-
 
 
 type FilterFlags<Base, Condition> = {
@@ -40,15 +46,15 @@ type SubType<Base, Condition> =
 
 type QueryProps<T> = Partial<OmitMethod<{
     [key in keyof T]: 
-        T[key] extends ComputeFunction? Parameters<T[key]>[1]: 
+        T[key] extends ComputeFunction? (Parameters<T[key]>[1] extends QueryX<infer I>? QueryX<I>: Parameters<T[key]>[1] ): 
         T[key] extends PropertyType<infer R> ? R:
         never;
-}> | {[key: string]: Scalar<any> }>
+}>>
 
 
 type ObjectValue<T> = OmitMethod<{
     [key in keyof T]: 
-        T[key] extends ComputeFunction? ReturnType<T[key]>:
+        T[key] extends ComputeFunction? (ReturnType<T[key]> extends Scalar<infer S> ? S: never):
         T[key] extends PropertyType<infer R> ? R:
         T[key];
 }>
@@ -57,48 +63,80 @@ class PropertyType<T>{
 
 }
 
-type QueryOption<T> = {
-    props?: QueryProps<T>
-    removeProps?: string[]
+// type QueryOption<T extends Schema> = {
+//     props?: QueryProps<T>
+// }
+
+class Entity {
+
 }
 
-
 class Schema {
-    static find<I extends Schema>(this: typeof Schema & (new (...args: any[]) => I), option: QueryOption<I>): ObjectValue<I>{
+    static find<I extends Schema, X extends Schema>(this: typeof Schema & (new (...args: any[]) => I), options: QueryX<X> ): ObjectValue<X>{
         throw new Error()
+    }
+
+    static entityClass<I extends Schema>(this: typeof Schema & (new (...args: any[]) => I) ){
+
+        const s = this
+
+        let entityClass = class extends Entity{
+            constructor(){
+                super()
+                //TODO copy attributes  
+            }
+        }
+        //TODO
+        return entityClass
     }
 }
 
 class ProductSchema extends Schema{
     name: PropertyType<string>
-    shop: ComputeFunction<QueryOption<ShopSchema>, ObjectValue<ShopSchema> > = (s: any, args: QueryOption<ShopSchema>) => {
-        throw new Error('')
-    }
-    // shop: ComputeFunction<QueryOption<ShopSchema>, ObjectValue<PropsFromQuery<ShopSchema, QueryOption<ShopSchema>>> > = (s: any, args: QueryOption<ShopSchema>) => {
+    // shop: ComputeFunction<QueryOption<ShopSchema>, ObjectValue<ShopSchema> > = (s: any, args: QueryOption<ShopSchema>) => {
     //     throw new Error('')
     // }
-    myABC: ComputeFunction<number, boolean > = (s: any, args) => {
-        throw new Error('')
+    // myABC: ComputeFunction<number, boolean > = (s: any, args) => {
+    //     throw new Error('')
+    // }
+    shop<I extends ShopSchema>(root: any, args: QueryX<I>): Scalar<ObjectValue<I>>{
+        throw new Error()
+
     }
+    myABC(root: any, args: number): Scalar<string> {
+         throw new Error()
+    }
+}
+
+class Dual extends Schema {
+    [key:string]: any
 }
 
 class ShopSchema extends Schema{
     name: PropertyType<string>
-    products: ComputeFunction<QueryOption<ProductSchema>, ObjectValue<ProductSchema>[]> = (s: any, args: QueryOption<ProductSchema>) => {
-        throw new Error('')
-    }
-    // products: ComputeFunction<QueryOption<ProductSchema>, ObjectValue<PropsFromQuery<ProductSchema, QueryOption<ProductSchema>>>[]> = (s: any, args: QueryOption<ProductSchema>) => {
+    // products: ComputeFunction<QueryOption<ProductSchema>, ObjectValue<ProductSchema>[]>
+    //  = (s: any, args: QueryOption<ProductSchema>) => {
     //     throw new Error('')
     // }
+    products<I extends ShopSchema>(root: any, args: QueryX<ProductSchema> ): Scalar<ObjectValue<I>> {
+         throw new Error()
+    }
 }
 
-let b : QueryOption<ProductSchema> = {
+let b : QueryX<ProductSchema> = {
     props: {
+        name: 'eee',
         myABC: 5,
         shop: {
             props: {
                 products: {
-
+                    props: {
+                        name: 'ee',
+                        // kite: 5
+                    },
+                    schema: class extends ProductSchema {
+                        kite: PropertyType<string>
+                    }
                 }
             }
         }
@@ -111,21 +149,41 @@ let b : QueryOption<ProductSchema> = {
 
 let result = ProductSchema.find(b)
 
-type ReplaceCompute<T> = {
-    [key in keyof T ]: 
-        T[key] extends ComputeFunction?  ReturnType<T[key]> :
-        T[key] extends PropertyType<infer R> ? R:
-        T[key];
+
+interface QueryX<S extends Schema>{
+    props: QueryProps<S>, 
+    schema?: typeof Schema & (new (...args: any[]) => S) 
 }
 
-type Comp = <S, Z, R = Z extends infer C? Pick<Z & S, AllowedNames<C, string | number>> : never>(arg: Z, schema: S) => R;
+let dynamic: <I extends Schema>(option: QueryX<I>) => QueryX<I>
 
+let z = dynamic({
+            props: {
+                aaa: 'xx',
+                products: {
+                    props: {
+                        name: ''
+                    }
+                }
+            },
+            schema: class B extends ShopSchema{
+                aaa: PropertyType<string>
+            }
+        })
+// let xx = (new AA())
 
-let c: Comp
-let z = c({s: 5, name: 33}, class extends Schema{
-    name: PropertyType<string>
-    my: PropertyType<string>
+let x = dynamic({
+    props: {
+        shop: z
+    },
+    schema: class AA extends ProductSchema{
+        kkk: PropertyType<string>
+    }
 })
+
+let a = ProductSchema.find(x)
+
+
 
 
 // All fields are not undefined
@@ -135,4 +193,9 @@ let z = c({s: 5, name: 33}, class extends Schema{
 
 
 // let a :RemoveQueryProps<ProductSchema>
+
+
+// type CCC = {
+//     [key in keyof ProductSchema] 
+// }
 
