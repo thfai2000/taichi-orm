b4dbe7d4651805c5d276c31fcf84ded939efbca5
Eddie Tang <thfai2000@gmail.com>
2021-12-10 02:24:37 +0800

    try playground

---
+++ docs/.vuepress/components/Playground.vue
@@ -0,0 +1,25 @@
+<template>
+    <iframe
+    frameborder="1"
+    width="100%"
+    height="500px"
+    :src="`//unpkg.com/javascript-playgrounds@^1.0.0/public/index.html${hashString}`"
+    >
+    </iframe>
+</template>
+
+<script setup lang="ts">
+// import { onMounted, onUpdated, onUnmounted } from 'vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+  parameters: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const hashString = computed(
+    () => '#data=' + encodeURIComponent(JSON.stringify(props.parameters))
+)
+</script>
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from 'vuepress'
+import { path } from '@vuepress/utils'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
@@ -6,6 +7,9 @@ export default defineUserConfig<DefaultThemeOptions>({
   title: 'Taichi ORM',
   base: '/taichi-orm/',
   description: 'A developer-friendly SQL ORM',
+  markdown: {
+    customComponent: undefined
+  },
   themeConfig: {
     repo: 'thfai2000/taichi-orm',
     navbar: [
@@ -20,52 +24,55 @@ export default defineUserConfig<DefaultThemeOptions>({
       //   children: ['/group/foo.md', '/group/bar.md'],
       // }
     ],
-    sidebar: {
-      '/guide/': [
-        {
-          text: 'Guide',
-          children: [
-            '/guide/README.md',
-            '/guide/getting-started.md',
-            {
-              text: 'Concepts',
-              children: [
-                '/guide/concepts/orm',
-                '/guide/concepts/database-context',
-                '/guide/concepts/schema',
-                '/guide/concepts/model',
-                '/guide/concepts/property',
-                '/guide/concepts/property-type',
-                '/guide/concepts/relations',
-                '/guide/concepts/model-api',
-                '/guide/concepts/query-builder'
-              ]
-            },
-            {
-              text: 'Query Examples',
-              children: [
-                '/guide/query-examples/operators'
-              ]
-            },
-            '/guide/typescript-support.md',
-            {
-              text: 'Use Cases',
-              children: [
-                '/guide/use-cases/scalar-transform',
-                '/guide/use-cases/access-control'
-              ]
-            }
-          ]
-        },
-      ],
-      '/reference/': [
-        {
-          text: 'Reference',
-          children: [
-            '/reference/scalar.md', 
-            '/reference/config.md'],
-        },
-      ],
-    },
+    
+    sidebar: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+        children: [
+          '/guide/README.md',
+          '/guide/getting-started.md',
+          {
+            text: 'Concepts',
+            children: [
+              '/guide/concepts/orm',
+              '/guide/concepts/database-context',
+              '/guide/concepts/schema',
+              '/guide/concepts/model',
+              '/guide/concepts/property',
+              '/guide/concepts/property-type',
+              '/guide/concepts/relations',
+              '/guide/concepts/model-api',
+              '/guide/concepts/query-builder'
+            ]
+          },
+          {
+            text: 'Query Examples',
+            children: [
+              '/guide/query-examples/operators'
+            ]
+          },
+          '/guide/typescript-support.md',
+          {
+            text: 'Use Cases',
+            children: [
+              '/guide/use-cases/scalar-transform',
+              '/guide/use-cases/access-control'
+            ]
+          }
+        ]
+      },
+      
+      {text: 'Playground', link: '/playground'},
+      
+      {
+        text: 'Reference',
+        link: '/reference/',
+        children: [
+          '/reference/scalar.md', 
+          '/reference/config.md'],
+      },
+      
+    ],
   },
 })
